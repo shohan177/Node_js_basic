0d58fdcbd2cc72c0c6eb9c38d39db9498681a84e
Sarwar Jahan Shohan <sarwar.shohan24@gmail.com>
2022-07-26 14:13:11 +0600

    file system module

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-//modulejs ES6
-// import { ageCal } from "./function.js"
-// console.log("hello", ageCal(20))
+//commomjs type
+// const fs = require('fs');
 
-//commonjs ES5
-const fun = require('./function');
-console.log(fun)
+//moduler type
+import { writeFile, writeFileSync, appendFileSync, readFileSync, renameSync, unlink } from 'fs'
+
+//async type 
+writeFile('filename.text', 'hi node you are awesome', (err, data) => {
+    console.log(err)
+})
+
+//sync type
+writeFileSync('filenameSync.text', 'hi this sync file sytem')
+
+//add in same file 
+appendFileSync('filenameSync.text', '\nwe want to learn more')
+
+// renameSync('file_location','rename')
+
+//delete file
+unlink('./filename.text', (err) => {
+    console.log(err)
+})
+
+//read file sync
+let data = readFileSync('./db.json')
+
+//buffer system data return 
+console.log(data)
+
+//buffer convert into string
+console.log(data.toString())
